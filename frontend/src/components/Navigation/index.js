a1d6526bf3ad2ae6f94a1c3490aed1b801c065ad
James Aikens <jaikens19@hotmail.com>
2021-02-28 20:24:32 -0600

    Fixed buttons on dashboard

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,11 +3,14 @@ import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import useDarkMode from "use-dark-mode";
+import feather from '../../images/feather.svg'
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
     const history = useHistory()
     const [search, setSearch] = useState('')
+    const darkMode = useDarkMode(false);
 
     function updateSearch(event) {
         setSearch(event.target.value.toUpperCase())
@@ -36,9 +39,7 @@ function Navigation({ isLoaded }) {
 
     return (
       <div className="navbar">
-        <NavLink exact to="/">
-         Home
-        </NavLink>
+        <NavLink className="logo" exact to="/"></NavLink>
         <div className="navbar-search-bar">
           <input
             type="text"
@@ -50,7 +51,20 @@ function Navigation({ isLoaded }) {
         </div>
         <div>
           <div className="light-mode-dark-mode-toggle">
-            <button>toggle</button>
+            <button
+              className="light-mode-button"
+              type="button"
+              onClick={darkMode.disable}
+            >
+              Light Mode
+            </button>
+            <button
+              className="dark-mode-button"
+              type="button"
+              onClick={darkMode.enable}
+            >
+              Dark Mode
+            </button>
           </div>
           {isLoaded && sessionLinks}
         </div>
